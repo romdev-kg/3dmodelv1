ed51bd8955fb65b84ab1a786744b58710124c667
romdev-kg <djorobaevramazan06@gmail.com>
2024-09-14 15:13:22 +0600

    123

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Generate3DModel from './Generate3DModel';
+
+function App() {
+    return (
+        <div className="App">
+            <Generate3DModel />
+        </div>
+    );
+}
+
+export default App;
